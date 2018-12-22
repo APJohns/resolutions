9da5f75bfcb6d06b5d1de1cb0d259b7d1dda8b1c
Ash Johns <ashleypjohns@gmail.com>
2018-12-22 00:26:20 -0500

    add touch support for deleting resolutions

--- src/components/Resolution.js
+++ src/components/Resolution.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 class ResItem extends React.Component {
+  state = {
+    toggle: false
+  };
+
   handleDelete = () => {
     if (this.props.isOwner) {
       this.props.deleteRes(this.props.index);
@@ -9,10 +13,16 @@ class ResItem extends React.Component {
 
   render() {
     return (
-      <li>
+      <li
+        className={this.state.toggle && "activeLi"}
+        onClick={() => this.setState({ toggle: !this.state.toggle })}
+      >
         <p>{this.props.resolution}</p>
         {this.props.isOwner && (
-          <button className="delete" onClick={this.handleDelete}>
+          <button
+            className={`${this.state.toggle && "activeDel"} delete`}
+            onClick={this.handleDelete}
+          >
             &times;
           </button>
         )}
