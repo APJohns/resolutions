1f8e2df9d161a1378c142e973035db66b6b250f5
Ash Johns <ashleypjohns@gmail.com>
2018-12-22 11:43:54 -0500

    Add logos for share buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Login from "./Login";
+import facebook from "../images/Facebook.svg";
+import twitter from "../images/Twitter.svg";
 
 const Header = props => (
   <nav>
     <Link to="/" className="logo">
       Resolutions
     </Link>
-    <a
-      href={`https://www.facebook.com/sharer/sharer.php?u=sickresolutions.com/resolutions/${
-        props.index
-      }`}
-    >
-      Facebook
-    </a>
-    <a
-      href={`https://twitter.com/home?status=Check%20out%20my%20resolutions%20this%20year!%20sickresolutions.com/resolutions/${
-        props.index
-      }`}
-    >
-      Twitter
-    </a>
-    {props.loggedOut ? (
-      <Login authenticate={props.authenticate} />
-    ) : (
-      <button className="logout" onClick={props.logout}>
-        Log Out
-      </button>
-    )}
+    <div className="rightNav">
+      <a
+        className="share"
+        href={`https://www.facebook.com/sharer/sharer.php?u=sickresolutions.com/resolutions/${
+          props.index
+        }`}
+      >
+        <img src={facebook} alt="Share on Facebook" className="shareImg" />
+      </a>
+      <a
+        className="share"
+        href={`https://twitter.com/home?status=Check%20out%20my%20resolutions%20this%20year!%20sickresolutions.com/resolutions/${
+          props.index
+        }`}
+      >
+        <img src={twitter} alt="Share on Twitter" className="shareImg" />
+      </a>
+      {props.loggedOut ? (
+        <Login authenticate={props.authenticate} />
+      ) : (
+        <button className="logout" onClick={props.logout}>
+          Log Out
+        </button>
+      )}
+    </div>
   </nav>
 );
 
